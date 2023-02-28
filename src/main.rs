59fcf6400bf6e603a325de4e8141d5fa7b1d6bbe
Reilly Wood <reilly.wood@icloud.com>
2023-02-28 11:48:59 -0800

    Add colours and hex nibble line

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use argh::FromArgs;
-use dialoguer::{theme::ColorfulTheme, Input, Select};
+use dialoguer::{theme::ColorfulTheme, Confirm, Input, Select};
+use owo_colors::OwoColorize;
 
 use hex::decode;
 
@@ -11,7 +12,7 @@ struct Args {}
 fn main() -> Result<()> {
     let _args: Args = argh::from_env();
 
-    let input: String = Input::new()
+    let input: String = Input::with_theme(&ColorfulTheme::default())
         .with_prompt("Gimme a hex string")
         .interact_text()?;
 
@@ -23,7 +24,12 @@ fn main() -> Result<()> {
         .interact()?;
 
     let bits_per_line = items[selection];
-    // let bytes_per_line = 8;
+    // let bits_per_line = 32;
+
+    let print_hex_lines = Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt("Display hex values on their own line?")
+        .default(true)
+        .interact()?;
 
     // let input = "308191060C2A8648";
     // let input = "308191060C2A8648308191060C2A8648";
@@ -31,19 +37,45 @@ fn main() -> Result<()> {
     // println!("Input: 0x{input}");
 
     let mut bits_processed = 0;
-    let mut _bytes_processed = 0;
+    let mut bits = vec![];
+    let mut nibbles = vec![];
 
     for byte in bytes {
-        let mut bits = vec![];
+        let nibble1 = byte >> 4;
+        nibbles.push(nibble1);
+        let nibble2 = byte & 0b00001111;
+        nibbles.push(nibble2);
+
         for i in (0..8).rev() {
             let mask = 1 << i;
             let bit_is_set = (mask & byte) > 0;
-            let bit = if bit_is_set { 1u8 } else { 0u8 };
+            let bit = u8::from(bit_is_set);
             bits.push(bit);
         }
+    }
 
-        for bit in bits {
-            print!("{bit}");
+    let bit_lines = bits.chunks(bits_per_line);
+    let nibbles_per_line = bits_per_line / 4;
+    let nibble_lines = nibbles.chunks(nibbles_per_line);
+
+    let zipped = bit_lines.zip(nibble_lines);
+
+    println!();
+    for (bit_line, nibble_line) in zipped {
+        if print_hex_lines {
+            for nibble in nibble_line {
+                let formatted = nibble_to_string(*nibble);
+                print!("{}    ", formatted.green())
+            }
+            println!();
+        }
+
+        for bit in bit_line {
+            if bit == &1 {
+                print!("{}", bit.blue());
+            } else {
+                print!("{bit}",);
+            }
             bits_processed += 1;
 
             if (bits_processed % 4) == 0 {
@@ -51,12 +83,16 @@ fn main() -> Result<()> {
             }
         }
 
-        _bytes_processed += 1;
-
-        if (bits_processed % bits_per_line) == 0 {
-            println!();
-        }
+        println!();
     }
 
     Ok(())
+}
+
+fn nibble_to_string(nibble: u8) -> String {
+    let binding = hex::encode_upper([nibble]);
+    let mut chars = binding.chars();
+    // skip first char, hex::encode always adds a 0
+    chars.next();
+    chars.as_str().to_string()
 }
